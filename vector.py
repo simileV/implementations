bff56f6590a32d8ea79fce7bfadad785e179bacb
--replace-all <bergjones@gmail.com>
2020-03-21 20:54:39 -0400

    typed in vector, need to start testing

--- vector.py
+++ vector.py
@@ -6,13 +6,8 @@ class Vector():
     def __init__(self):
         self.size = 0
         self.capacity = 1
-        self.arr = [0] * 1
-
-    def size(self):
-        return self.size
-
-    def capacity(self):
-        return self.capacity
+        # self.arr = [0] * 1
+        self.arr = [None] * self.capacity
 
     def is_empty(self):
         if self.capacity == 0:
@@ -23,6 +18,15 @@ class Vector():
         if index < 0 or index >= self.size:
             return IndexError("index is out of bounds")
         return self.arr[index]
+
+    # def resize(self, new_capacity):
+    #     biggerArray = [0] * new_capacity
+    #
+    #     for i in range(self.size):
+    #         biggerArray[i] = self.arr[i]
+    #
+    #     self.arr = biggerArray
+    #     self.capacity = new_capacity
 
     def resize(self, new_capacity):
         biggerArray = [0] * new_capacity
@@ -44,7 +48,7 @@ class Vector():
         if index < 0 or index >= self.size:
             return IndexError("index is out of bounds")
 
-        if self.size + 1 > self.capacity():
+        if self.size + 1 >= self.capacity:
             self.resize(self.capacity * 2)
 
         temp = self.arr[index]
@@ -55,20 +59,41 @@ class Vector():
         for i in range(self.capacity, index, -1):
             self.arr[i + 1] = self.arr[i]
 
-    def insert(self, item):
+    def prepend(self, item):
+        """add an element to the beginning of the list."""
         self.insert(0, item)
 
     def pop(self):
-        pass
+        self.arr[-1] = None
+
+        if self.size == (self.capacity / 4):
+            self.resize(self.capacity / 2)
 
     def delete(self, index):
-        pass
+        """delete item at index, shifting all trailing elements left"""
+        if index < 0 or index >= self.size:
+            return IndexError("index is out of bounds")
 
-    def remove(self):
-        pass
+        self.arr[index] = None
+
+        for i in range(index, self.capacity):
+            self.arr[i] = self.arr[i + 1]
+
+    def remove(self, item):
+        idxToDel = -1
+        for i in range(self.size):
+            if item == self.arr[i]:
+                idxToDel = i
+
+        self.delete(idxToDel)
 
     def find(self, item):
-        pass
+        foundIdx = -1
+
+        for i in range(self.size):
+            if item == self.arr[i]:
+                return i
+        return foundIdx
 
 # new raw data array
 
