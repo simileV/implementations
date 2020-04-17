182fa529a133067834950a997632bac8be48e0c4
--replace-all <bergjones@gmail.com>
2020-04-17 11:51:33 -0400

    proxy

--- Design Patterns/proxy.py
+++ Design Patterns/proxy.py
@@ -11,3 +11,61 @@ the proxy pattern usages.
 
 """
 
+class Subject:
+    """
+    As mentioned in the document, interfaces of both RealSubject and Proxy should
+    be the same, because the client should be able to use RealSubject or Proxy with
+    no code change.
+    Not all times this interface is necessary. The point is the client should be
+    able to use RealSubject or Proxy interchangeably with no change in code.
+    """
+
+    def do_the_job(self, user):
+        raise NotImplementedError()
+
+
+class RealSubject(Subject):
+    """
+    This is the main job doer. External services like payment gateways can be a
+    good example.
+    """
+
+    def do_the_job(self, user):
+        print(f'I am doing the job for {user}')
+
+
+class Proxy(Subject):
+    def __init__(self):
+        self._real_subject = RealSubject()
+
+    def do_the_job(self, user):
+        """
+        logging and controlling access are some examples of proxy usages.
+        """
+
+        print(f'[log] Doing the job for {user} is requested.')
+
+        if user == 'admin':
+            self._real_subject.do_the_job(user)
+        else:
+            print(f'[log] I can do the job just for `admins`.')
+
+
+def client(job_doer, user):
+    job_doer.do_the_job(user)
+
+def main():
+    """
+    >>> proxy = Proxy()
+    >>> real_subject = RealSubject()
+    >>> client(proxy, 'admin')
+    [log] Doing the job for admin is requested.
+    I am doing the job for admin
+    >>> client(proxy, 'anonymous')
+    [log] Doing the job for anonymous is requested.
+    [log] I can do the job just for `admins`.
+    >>> client(real_subject, 'admin')
+    I am doing the job for admin
+    >>> client(real_subject, 'anonymous')
+    I am doing the job for anonymous
+    """
